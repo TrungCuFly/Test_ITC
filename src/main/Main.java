a36de378f2f6504966595e7346b326c068d51586
TrungCuFly <meo28092003@gmail.com>
2022-01-14 19:18:41 +0700

    ok2

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Trung đẹp trai");
+        System.out.println("Trung rất đẹp trai");
         Test_ITC t = new Test_ITC();
         t.inputArray();
         t.outputArray();
